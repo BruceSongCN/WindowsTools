b57d4c54e6de884b8ca54bc1a3bfb53b09e0a143
bruce <bruce_vs@163.com>
2016-06-16 13:44:15 +0800

    调整

--- DiskMonitor/Program.cs
+++ DiskMonitor/Program.cs
@@ -18,16 +18,17 @@ namespace DiskMonitor
 
         static void Main(string[] args)
         {
-            Console.WriteLine("磁盘空间检测程序启动...");
-
-            string strDiskSetting = System.Configuration.ConfigurationManager.AppSettings["DiskSetting"];
-            string strEmailSetting = System.Configuration.ConfigurationManager.AppSettings["EmailSetting"];
-            string strServerName = System.Configuration.ConfigurationManager.AppSettings["ServerName"];
-            strLogPath = System.Configuration.ConfigurationManager.AppSettings["LogPath"];
-            strLogName = "log";
-
             try
             {
+                Console.WriteLine("磁盘空间检测程序启动...");
+
+                string strDiskSetting = System.Configuration.ConfigurationManager.AppSettings["DiskSetting"];
+                string strEmailSetting = System.Configuration.ConfigurationManager.AppSettings["EmailSetting"];
+                strLogPath = System.Configuration.ConfigurationManager.AppSettings["LogPath"];
+                strLogName = "log";
+                // 邮箱配置项
+                string[] arrEmailSetting = strEmailSetting.Split(';');
+
                 foreach (string s in strDiskSetting.Split(','))
                 {
                     if (!string.IsNullOrEmpty(s))
@@ -40,22 +41,23 @@ namespace DiskMonitor
                         // 获取磁盘剩余空间（GB）
                         double freeSpace = CommonLib.DiskHelper.GetHardDiskFreeSpace(strDiskName);
                         // 打印记录信息
-                        ConsoleAndWriteLog(string.Format("成功检测到{0}盘的剩余空间为{1}GB", strDiskName, freeSpace));
+                        ConsoleAndWriteLog(string.Format("成功监测到{0}盘的剩余空间为{1}GB", strDiskName, freeSpace));
                         // 检测报警条件
                         if (freeSpace < minSpace)
                         {
-                            string strSmtpServer = strEmailSetting.Split(',')[0].Split(':')[1];
-                            string strUserName = strEmailSetting.Split(',')[1].Split(':')[1];
-                            string strPassword = strEmailSetting.Split(',')[2].Split(':')[1];
-                            string strMailTo = strEmailSetting.Split(',')[3].Split(':')[1];
-                            string strSubject = string.Format("服务器({0})磁盘空间报警", strServerName);
+                            string strSmtpServer = arrEmailSetting[0].Split(':')[1];
+                            string strUserName = arrEmailSetting[1].Split(':')[1];
+                            string strPassword = arrEmailSetting[2].Split(':')[1];
+                            string strMailTo = arrEmailSetting[3].Split(':')[1];
+                            string strServerName = arrEmailSetting[4].Split(':')[1];
+                            string strSubject = string.Format("【{0}】磁盘空间报警", strServerName);
                             string strBody = string.Format("您好，监测到目标服务器{0}盘剩余空间为{1}GB，已低于设定的报警阈值{2}GB，请及时处理！", strDiskName, freeSpace, minSpace);
                             CommonLib.SmtpHelper smtp = new CommonLib.SmtpHelper(strSmtpServer, strUserName, strPassword);
 
                             try
                             {
                                 // 发送报警邮件
-                                if (smtp.Send(strMailTo, strSubject, strBody))
+                                if (smtp.Send(strMailTo.Split(','), strSubject, strBody))
                                 {
                                     ConsoleAndWriteLog(string.Format("{0}盘剩余空间低于报警阈值{1}GB，已成功给{2}发送报警邮件！", strDiskName, minSpace, strMailTo));
                                 }
